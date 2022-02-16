3a8a3f65c532ebe4c1f2d7a1205ef13cedb55718
eaghayev <eaghayevv@gmail.com>
2022-02-16 16:53:56 +0400

    project added

---
+++ P223ShopProject/Shop.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Core.Repositories;
+
+namespace Shop.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+
+    }
+}
